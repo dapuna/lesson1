a099b13360aae52d6e88a1c4be030f1ca14ede96
dapuna <friends1001d@gmail.com>
2022-05-19 19:12:58 +0300

    dz3

---
+++ dz3.py
@@ -0,0 +1,18 @@
+class cat:
+    hp = 100
+    eat = 100
+    name = "Masha"
+    def __int__(self,n):
+        self.name = n
+        print(self.name,"hello")
+        self.live()
+        self.live()
+        self.eateng()
+    def eateng(self):
+        self.eat  += 10
+        print(self.eat)
+    def live(self):
+        self.eat -=5
+        print(self.eat)
+    def damsge(self):
+        self.ho -=25
