262b4fcd46bf11a357f633fb759f61aca38b7508
dapuna <friends1001d@gmail.com>
2022-05-21 10:44:38 +0300

    lesson5 Inheritance of classes

---
+++ lesson5-6.py
@@ -0,0 +1,12 @@
+class Bird:
+    color =0
+    def fly(self):
+        pass
+class Flamingo(Bird):
+    migration = True
+    def standOtOneLeg(self):
+        pass
+class Sparon(Bird):
+    def jumping(self):
+        pass
+
