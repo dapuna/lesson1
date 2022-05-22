33c495f085710f84f3c042406753c0eb6e51a93f
dapuna <friends1001d@gmail.com>
2022-05-22 19:46:13 +0300

    dz5

---
+++ dz5.py
@@ -0,0 +1,15 @@
+class Archons :
+    element = True
+    age = 0
+class Venty(Archons):
+    def freedom(self):
+        pass
+class Zhong_Li(Archons):
+    def contract(self):
+        pass
+class Reiden(Archons):
+    def saving(self):
+        pass
+
+
+
--- lesson5-6.py
+++ lesson5-6.py
@@ -1,12 +1,14 @@
-class Bird:
-    color =0
-    def fly(self):
-        pass
-class Flamingo(Bird):
-    migration = True
-    def standOtOneLeg(self):
-        pass
-class Sparon(Bird):
-    def jumping(self):
-        pass
+# class Bird:
+#     color =0
+#     def fly(self):
+#         pass
+# class Flamingo(Bird):
+#     migration = True
+#     def standOtOneLeg(self):
+#         pass
+# class Sparon(Bird):
+#     def jumping(self):
+#         pass
+
+
 
